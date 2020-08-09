3af3fdc518b1914b2092badabcd75351851ce3bc
Ricky199X <rickyrojas19@gmail.com>
2020-08-09 15:06:57 -0400

    user auth GET successful + tested

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../../middleware/auth')
+const User = require('../../models/User')
 
 // Route: api/auth -> test auth route
 // Public
-router.get('/', (req, res) => res.send('auth route'))
-
+router.get('/', auth, async (req, res) => {
+    try {
+        // in our middle ware, we set the user to the user in the token, so we can access the user obj
+        const user = await User.findById(req.user.id).select('-password')
+        res.json(user)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
 
 module.exports = router 
