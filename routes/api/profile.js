8b97d51031533c6cf26fda65ed6467c1e8f56a25
Ricky199X <rickyrojas19@gmail.com>
2020-08-13 11:15:52 -0400

    added route to get a profile by associated user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -121,5 +121,24 @@ router.post('/', [
     }
 )
 
+// Route: GET api/profile/user/:user_id -> route to get profile by User Id
+// Public
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        // find the logged in user by ID - also bring in their name and avatar
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar'])
+
+        if (!profile) {
+            return res.status(400).json({ msg: `That user does not exist!` })
+        }
+
+        res.json(profile)
+
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send(`Server Err`)
+    }
+})
+
 
 module.exports = router 
