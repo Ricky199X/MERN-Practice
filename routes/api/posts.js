ff1a7046b6f495ff301417c8e26b16899c38c14f
Ricky199X <rickyrojas19@gmail.com>
2020-08-14 23:10:50 -0400

    routes added to get all posts and add a post - both tested successfully

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,61 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult } = require('express-validator')
+const auth = require('../../middleware/auth')
+
+const Post = require('../../models/Post')
+const Profile = require('../../models/Profile')
+const User = require('../../models/User')
 
 // Route: api/posts -> test route to get all posts
 // Public
-router.get('/', (req, res) => res.send('posts route'))
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find()
+
+        if (!posts) {
+            return res.status(400).json({ msg: `Cannot find posts!` })
+        }
+
+        res.json(posts)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send(`Server Error`)
+    }
+})
+
+// Route: api/posts -> route to add a post
+// Private
+
+router.post('/', [auth, [
+    check('text', 'Text is required').not().isEmpty()
+]], async (req, res) => {
+
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    try {
+        const user = await User.findById(req.user.id).select('-password')
+
+        const newPost = new Post({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        })
+
+        const post = await newPost.save()
+        res.json(post)
+
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send(`Server Error`)
+    }
+
+})
 
 
 module.exports = router 
