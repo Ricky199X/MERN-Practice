2fcf9d8b924a521ebedfb035c6c4c4ded37acd88
Ricky199X <rickyrojas19@gmail.com>
2020-08-16 16:03:17 -0400

    unlike post added + tested

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -139,4 +139,36 @@ router.put('/like/:id', auth, async (req, res) => {
     }
 })
 
+// Route: PUT api/posts/unlike/:id -> route to remove a like to a post
+// Private
+
+router.put('/unlike/:id', auth, async (req, res) => {
+
+    try {
+        const post = await Post.findById(req.params.id)
+
+        // check if the post has already been unliked
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+            return res.status(400).json({ msg: `Post has not yet been liked!` })
+        }
+
+        // get the remove index
+        const removeIndex = post.likes.map((like) => like.user.toString()).indexOf(req.user.id)
+
+        post.likes.splice(removeIndex, 1)
+
+        await post.save()
+        res.json(post.likes)
+
+    } catch (err) {
+        console.error(err.message)
+
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: `Post not found!` })
+        }
+
+        res.status(500).send(`Server Error`)
+    }
+})
+
 module.exports = router 
