360fd80a1ab22a957464f119669d3f490071614d
Ricky199X <rickyrojas19@gmail.com>
2020-08-25 17:07:19 -0400

    updated server port to port 5000

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 // connect database
 connectDB()
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 
